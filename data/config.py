4bc29fadac15fb44e78ba44da309e14fa1cf38de
Dragon_Fly_02 <vuphanthanglong@gmail.com>
2021-04-22 21:23:20 +0200

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,5 @@
+features = ['pickup_longitude', 'pickup_latitude',
+       'dropoff_longitude', 'dropoff_latitude','pickup_day', 'pickup_time', 'pickup_dayofweek', 'pickup_weekday',
+       'displacement', 'direction', 'temperature', 'humidity', 'wind_speed',
+       'wind_direction', 'pickup_pca0', 'pickup_pca1', 'dropoff_pca0',
+       'dropoff_pca1','total_distance']
